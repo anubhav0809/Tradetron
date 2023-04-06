e50bd6ef02f62049eef94591a514afbb8a1139d5
Anubhav <anubhavsaturday@gmail.com>
2023-04-06 23:32:09 +0530

    Newsfeed added

---
+++ src/Newsfeed.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Newsfeed.css'
+import LineGraph from './LineGraph'
+import TimeLine from './TimeLine'
+
+function Newsfeed() {
+    return (
+        <div className="newsfeed">
+            <div className="newsfeed__container">
+                <div className="newsfeed__container">
+                    <div className="newsfeed__chartSection">
+                        <div className="newsfeed__portfolio">
+                            <h1>$114,656</h1>
+                            <p>+$44.63 (+0.04%) Today</p>
+                        </div>
+                        <div className="newsfeed__chart">
+                            <LineGraph />
+                            <TimeLine />
+                        </div>
+                    </div>
+                    <div className="newsfeed__buying__section">
+                        <h2> Buying Power</h2>
+                        <h2> $4.11</h2>
+                    </div>
+
+                    <div className="newsfeed__market__section">
+                        <div className="newsfeed__market__box">
+                            <p> Markets Closed</p>
+                            <h1> Happy Thanksgiving</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Newsfeed
